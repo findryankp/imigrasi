90f0a47d617d793ac07324e8de9d0b0033b55896
Unknown <findryankurnia@gmail.com>
2018-01-03 09:13:17 +0700

    Make pelaporan view

---
+++ resources/views/master/master.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon.png">
+    <title>@yield('title')</title>
+
+    @yield('head')
+    <!-- Bootstrap Core CSS -->
+    <link href="assets/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- This page CSS -->
+    <!-- Custom CSS -->
+    <link href="wrapkit/css/style.css" rel="stylesheet">
+    <link href="wrapkit/css/index-landingpage/landing-page.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body class="">
+    @yield('content')
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    @yield('script')
+    <script src="assets/node_modules/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap popper Core JavaScript -->
+    <script src="assets/node_modules/popper/dist/popper.min.js"></script>
+    <script src="assets/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!--Custom JavaScript -->
+    <script src="wrapkit/js/custom.min.js"></script>
+    <!-- ============================================================== -->
+    <script type="text/javascript">
+        
+        $('a').on('click', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 90
+            }, 1000);
+            event.preventDefault();
+            // code 
+            
+        });
+    </script>
+</body>
+
+</html>
---
+++ resources/views/pelaporan.blade.php
@@ -0,0 +1,66 @@
+@extends('master.master')
+@section('title') Pelaporan
+@stop
+@section('head')
+@stop
+
+@section('content')
+    <div id="main-wrapper">
+        <!-- Page wrapper  -->
+        <!-- ============================================================== -->
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <div class="static-slider10" style="background-image:url(assets/images/slide_12_1463126003.png)">
+                    <div class="container">
+                        <!-- Row  -->
+                        <div class="row fade-left-content-center ">
+                            <!-- <a class="btn btn-outline-light btn-rounded btn-md btn-arrow m-t-20" data-toggle="collapse" href=""><h4><span>Pelaporan <i class="ti-arrow-right"></i></span></h4></a> -->
+                            
+                            <h5 class="title responsive">  Pelaporan</h5>
+                            <form data-aos="fade-left" data-aos-duration="1200">
+                                <div class="row">
+                                    <div class="col-md-12 col-lg-12 col-sm-8">
+                                        <div class="form-group m-t-15">
+                                            <img src="assets/images/team/t3.jpg" class="img-responsive">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12 col-lg-12 col-sm-8">
+                                        <div class="form-group m-t-15">
+                                            <input style="opacity: 0.7;" class="form-control" type="text" placeholder="nomer HP">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12 col-lg-12 col-sm-8">
+                                        <div class="form-group m-t-15">
+                                            <input style="opacity: 0.7;" class="form-control" type="text" placeholder="alamat" disabled>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12 col-lg-12 col-sm-8">
+                                        <div class="form-group m-t-15">
+                                            <textarea style="opacity: 0.7;" class="form-control" rows="3" placeholder="keterangan"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12 col-lg-12 col-sm-8">
+                                        <button type="submit" class="btn btn-success-gradiant font-17 btn-arrow"><span> Laporkan <i class="ti-arrow-right"></i></span></button>
+                                    </div>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- ============================================================== -->
+            </div>
+            <!-- ============================================================== -->
+            <a class="bt-top btn btn-circle btn-lg btn-info" href="#top"><i class="ti-arrow-up"></i></a>
+        </div>
+        
+        <!-- ============================================================== -->
+    </div>
+@stop
+
+@section('script')
+@stop
